53d44c2d3877edea4ec4144746496ec0af02fd3b
Mohammed Bilal N S <70318383+mohammedbilalns@users.noreply.github.com>
2024-05-10 20:16:42 +0530

    updated

--- notes/urls.py
+++ notes/urls.py
@@ -4,10 +4,10 @@ from .import views
 app_name = 'notes'
 urlpatterns = [
     path('', views.notes_list, name="list"),
-    path('', views.notes_list, name="list"),
-    path('', views.notes_list, name="list"),
-
-
+    path('plusone/', views.plusone, name="plusone"),
+    path('plustwo/', views.plustwo, name="plustwo"),
+    path('linearalgebra/', views.linearalgebra, name="linearalgebra"),
+    path('<slug:slug>', views.notespage, name="page"),
 
 
   
